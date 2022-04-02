64b5342635858f98c9a65bbdbd2401ca42c781fd
Jim Shi <shiyilin2008@gmail.com>
2022-04-01 17:07:04 -0700

    [Minor] Lossless image conversion: Rename argument

--- convert_image_lossless.py
+++ convert_image_lossless.py
@@ -8,10 +8,10 @@ from PIL import Image
 OLD_EXTENSION: typing.Final = ".old"
 
 
-def get_image_filenames(root_dir: str, image_extensions: typing.List[str]) -> typing.List[str]:
+def get_image_filenames(src_dir: str, image_extensions: typing.List[str]) -> typing.List[str]:
     return_value = []
 
-    for current_dir, dir_names, filenames in os.walk(root_dir):
+    for current_dir, dir_names, filenames in os.walk(src_dir):
         for filename in filenames:
             match = any((filename.endswith(ext) for ext in image_extensions))
             if match:
@@ -34,18 +34,18 @@ def convert_image_to_webp(src_filename: str, dest_filename: str):
     src_image.save(dest_filename, lossless=True, quality=100, method=6)
 
 
-def main(root_dir: str, assume_yes: bool, input_extensions: typing.List[str], output_extension: str):
+def main(src_dir: str, assume_yes: bool, input_extensions: typing.List[str], output_extension: str):
     # Check input directory.
-    print(f"Using root dir `{root_dir}`.")
-    if not os.path.isdir(root_dir):
-        raise NotADirectoryError(f"`{root_dir}` isn't a dir!")
+    print(f"Using source dir `{src_dir}`.")
+    if not os.path.isdir(src_dir):
+        raise NotADirectoryError(f"`{src_dir}` isn't a dir!")
 
     # Add . before extensions.
     input_extensions = ["." + ext for ext in input_extensions]
     output_extension = "." + output_extension
 
     # List files to convert.
-    image_filenames = get_image_filenames(root_dir, input_extensions)
+    image_filenames = get_image_filenames(src_dir, input_extensions)
     if not image_filenames:
         print("No file to convert.")
         exit(0)
@@ -74,10 +74,10 @@ def main(root_dir: str, assume_yes: bool, input_extensions: typing.List[str], ou
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--root_dir", type=str, default=os.getcwd())
+    parser.add_argument("--src_dir", type=str, default=os.getcwd())
     parser.add_argument("--assume_yes", "-y", action="store_true")
     parser.add_argument("--input_extensions", nargs="+", default=["png"])
     parser.add_argument("--output_extension", type=str, default="webp")
     args = parser.parse_args()
 
-    main(args.root_dir, args.assume_yes, args.input_extensions, args.output_extension)
+    main(args.src_dir, args.assume_yes, args.input_extensions, args.output_extension)
